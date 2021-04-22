515ae1b670be35183149c366f4c24693a140a1ff
HovhannesTutyan <73821307+HovhannesTutyan@users.noreply.github.com>
2021-04-22 17:23:01 +0400

    Delete main.py

--- main/main.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome(executable_path='./chromedriver')
-driver.get('https://www.google.com')
-driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("Yahoo")
-driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[2]/div[2]/div[2]/center/input[1]").click()
